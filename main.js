2772599662dcb22957b553ba901d2ca02921344a
Paulina Rutkowska <polciaalicious@gmail.com>
2021-11-24 13:39:17 +0100

    before week 6

--- main.js
+++ main.js
@@ -2,3 +2,6 @@ const firstName = "Paulina";
 const age = 32;
 
 console.log(`Siema, nazywam sie ${firstName} i mam ${age} lata.`);
+
+const add = document.querySelector('.js-add');
+add.innerHTML = "tylko dla ciebie i twoich najblizszych"
