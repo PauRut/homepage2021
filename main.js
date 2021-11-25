26bb38f3ddbe75c94514458d0d9e9d4a4ac60682
Paulina Rutkowska <polciaalicious@gmail.com>
2021-11-25 11:30:45 +0100

    adding a new page called FORM

--- main.js
+++ main.js
@@ -1,7 +1,35 @@
 const firstName = "Paulina";
-const age = 32;
-
+const age = 31;
 console.log(`Siema, nazywam sie ${firstName} i mam ${age} lata.`);
 
-const add = document.querySelector('.js-add');
-add.innerHTML = "tylko dla ciebie i twoich najblizszych"
+
+// Funkcja strzalkowa
+
+const greet = (ages, secondName) => {
+    console.log("Witaj drogi Odwiedzajacy, nazywam sie ${firstName} i mam ${age} lata.");
+}
+greet (32, 'Paulina')
+
+// Dodawanie tekst do HTML za pomoca JS
+
+const heading = document.querySelector('.main__heading--js');
+heading.innerHTML = `ELooo drogi Odwiedzajacy, nazywam sie ${firstName} i mam ${age} lata.`;
+
+console.log(heading)
+
+
+// if 
+
+if (32 > 20) {
+    console.log('to prawda')
+} else { 
+    console.log('to nieprawda')
+}
+
+
+(32 >20) ? console.log('to prawda') : console.log("to niepawda");
+
+
+const result = (32 >20) ? true : false;
+
+console.log(result)
