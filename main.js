1330ae8e210337bfc5201f4519bbbcea43e1ef77
Paulina Rutkowska <polciaalicious@gmail.com>
2021-11-26 13:14:15 +0100

    hamburger menu

--- main.js
+++ main.js
@@ -2,34 +2,51 @@ const firstName = "Paulina";
 const age = 31;
 console.log(`Siema, nazywam sie ${firstName} i mam ${age} lata.`);
 
-
 // Funkcja strzalkowa
 
 const greet = (ages, secondName) => {
-    console.log("Witaj drogi Odwiedzajacy, nazywam sie ${firstName} i mam ${age} lata.");
-}
-greet (32, 'Paulina')
+  console.log(
+    "Witaj drogi Odwiedzajacy, nazywam sie ${firstName} i mam ${age} lata."
+  );
+};
+greet(32, "Paulina");
 
 // Dodawanie tekst do HTML za pomoca JS
 
-const heading = document.querySelector('.main__heading--js');
-heading.innerHTML = `ELooo drogi Odwiedzajacy, nazywam sie ${firstName} i mam ${age} lata.`;
+// const heading = document.querySelector('.main__heading--js');
+// heading.innerHTML = `ELooo drogi Odwiedzajacy, nazywam sie ${firstName} i mam ${age} lata.`;
 
-console.log(heading)
+// console.log(heading)
 
-
-// if 
+// if
 
 if (32 > 20) {
-    console.log('to prawda')
-} else { 
-    console.log('to nieprawda')
+  console.log("to prawda");
+} else {
+  console.log("to nieprawda");
 }
 
+32 > 20 ? console.log("to prawda") : console.log("to niepawda");
 
-(32 >20) ? console.log('to prawda') : console.log("to niepawda");
+const result = 32 > 20 ? true : false;
+console.log(result);
 
+// Change the text of heading by clicking on the button
+const button = document.querySelector(".action--js");
 
-const result = (32 >20) ? true : false;
+const myClick = () => {
+  const heading = document.querySelector(".main-heading--js");
+  heading.innerHTML = `Beautiful furnitures from  zywica, 
+    For your office and your home`;
+  heading.classList.toggle("change-background-js");
+};
+button.addEventListener("click", myClick);
 
-console.log(result)
+// hamburger menu
+
+const hamburger = document.querySelector(".hamburger--js");
+
+hamburger.addEventListener("click", () => {
+  const nav = document.querySelector(".navigation--js");
+  nav.classList.toggle("navigation--open");
+});
